2aef948898636766ca2c35eeb3b7ee5fe2033f49
Tony5897 <tony.martinez5897@gmail.com>
2022-10-20 21:35:39 -0700

    lists overview

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+// This is a JavaScript refreshment session using various examples//
+// appending, removing, splicing .pop//
+
+
+myList = ['rug','chair','desk']
+
+myList.append('lamp')
+
+// now, myList == ['rug', 'chair', 'desk', 'lamp'] //
+
+myList = ['strawberry', 'blackberry', 'blueberry']
+ 
+myList.splice(1, 0, 'cherry') // make 'cherry' the second item in the list
+ 
+// now, myList = ['strawberry', 'blackberry', 'blueberry', 'cherry'] //
+
+
+
+
+myList = ['rug','chair','desk']
+
+  // removes 'lamp' from myList and return 'lamp'//
+myList('lamp')
+
+  // now, myList == ['rug', ['chair']]
+
+myList = ['strawberry', 'blackberry', 'blueberry']
+ 
+myList.splice(1, 1,) // removes 'blackberry' from myList and returns 'banana' //  
+  // now, myList = ['strawberry', 'blueberry'// 
+
+
+
+  // Example Run: Building a list //
+
+  myList = ['dog', 'cat', 'parrot']
+  // myList[0] <--- This will return 'dog' //
+
+  // saving to a variable //
+
+states = ['oregon', 'california', 'texas', 'washington']
+  // states[2] <--- This will return 'texas' //
+
+  theWarmestState = states[2]
+
+
